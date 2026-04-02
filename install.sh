6f0fc94db30f65a2ec6d85fb064797e7bc9aba78
dirge <dirge@noreply.codeberg.org>
2026-04-03 00:30:29 +0200

    Update install.sh
    Changing tuigreet for nwg-hello.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 cp -r -i ./home/.* ~/
 
 # Installing software
-sudo pacman -Syu hyprland xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper mako fuzzel thunar waybar cliphist mousepad greetd-tuigreet wlogout grimblast-git hyprlock hypridle nwg-look materia-gtk-theme qt6ct kvantum kvantum-theme-materia breeze-icons playerctl network-manager-applet
+sudo pacman -Syu hyprland xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper mako fuzzel thunar waybar cliphist mousepad nwg-hello wlogout grimblast-git hyprlock hypridle nwg-look materia-gtk-theme qt6ct kvantum kvantum-theme-materia breeze-icons playerctl network-manager-applet
 
 # Installing some optional dependencies
 sudo pacman -S --asdeps uwsm catfish gvfs tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller otf-font-awesome hyprpicker
@@ -16,5 +16,5 @@ gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark"
 gsettings set org.gnome.desktop.interface icon-theme "breeze-dark"
 
 # Configuring and enabling greetd
-sudo cp ./greetd/config.toml /etc/greetd/config.toml
+sudo cp ./greetd/greetd.conf /etc/greetd/greetd.conf
 systemctl enable greetd
